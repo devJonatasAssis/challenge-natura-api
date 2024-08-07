b4b9a8c917c2efbdc3ffa697ad6978c29babb3fc
Jonatas de Assis Silva <devjonatasassis@gmail.com>
2024-08-07 00:38:55 -0300

    feature: changes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as heapdump from 'heapdump';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8080);
+
+  process.on('SIGUSR2', () => {
+    const filename = `/tmp/${Date.now()}.heapsnapshot`;
+    heapdump.writeSnapshot(filename, (err, filename) => {
+      if (err) {
+        console.error('Error writing snapshot:', err);
+      } else {
+        console.log('Heap snapshot written to', filename);
+      }
+    });
+  });
+
+  await app.listen(3333);
 }
 bootstrap();
